6277ba69e85b226674908166f4799f07624226a0
rit1992@mail.ru <158091165+margarita0206@users.noreply.github.com>
2024-07-15 07:52:15 +0300

    Задание Отрисуй меня полностью

--- js/data.js
+++ js/data.js
@@ -1,24 +1,23 @@
 import {
-  POSTS_COUNT, AVATAR_MIN_COUNT, AVATAR_MAX_COUNT, LIKES_MIN_COUNT, LIKES_MAX_COUNT, COMMENT_MIN_COUNT, COMMENT_MAX_COUNT, COMMENT_MAX_ID, COMENT_NAMES, POSTS_COMMENTS, DESCRIPTIONS
+  MAX_POSTS_COUNT, MIN_POSTS_COUNT, AVATAR_MIN_COUNT, AVATAR_MAX_COUNT, LIKES_MIN_COUNT, LIKES_MAX_COUNT, COMMENT_MIN_COUNT, COMMENT_MAX_COUNT, COMMENT_MAX_ID, COMMENT_NAMES, POSTS_COMMENTS, DESCRIPTIONS
 } from './constant.js';
 
 import {
   getRandomInteger, getUniqueInteger, getRandomArrayElement
 } from './utils.js';
 
-const randomPostId = getUniqueInteger(1, POSTS_COUNT);
-const randomPostUrl = getUniqueInteger(1, POSTS_COUNT);
+const randomPostId = getUniqueInteger(MIN_POSTS_COUNT, MAX_POSTS_COUNT);
+const randomPostUrl = getUniqueInteger(MIN_POSTS_COUNT, MAX_POSTS_COUNT);
 const randomCommentID = getUniqueInteger(1, COMMENT_MAX_ID);
 
 const createPost = () => {
-
   const randomPostDescription = getRandomArrayElement(DESCRIPTIONS);
 
   const createComment = () => ({
     'id': randomCommentID,
     'avatar': `img/avatar-${getRandomInteger(AVATAR_MIN_COUNT,AVATAR_MAX_COUNT)}.svg`,
     'message': getRandomArrayElement(POSTS_COMMENTS),
-    'name': getRandomArrayElement(COMENT_NAMES),
+    'name': getRandomArrayElement(COMMENT_NAMES),
   });
 
   return {
@@ -30,6 +29,6 @@ const createPost = () => {
   };
 };
 
-const randomPosts = Array.from({ length: POSTS_COUNT }, createPost);
+const randomPosts = () => Array.from({ length: MAX_POSTS_COUNT }, createPost);
 
 export {randomPosts};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import {randomPosts} from './data.js';
+import {generateThumbnails} from './thumbnail.js';
+generateThumbnails(randomPosts());
 
-console.log(randomPosts);
